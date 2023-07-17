f2f29ab1952d834c6d5e9a489cceb79e50cd11df
sarveshpatil1 <sarveshpatil0000@gmail.com>
2023-07-17 19:55:27 -0400

    added multiple html files

--- LMSapp/urls.py
+++ LMSapp/urls.py
@@ -1,11 +1,14 @@
-from . import views
+from . import views, user_login
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-    path('base', views.BASE,name='BASE')
-
+    path('base', views.BASE, name='BASE'),
+    path('', views.home, name='home'),
+    path('singlecourse', views.singlecourse, name='singlecourse'),
+    path('contactus', views.contactus, name='contactus'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register', user_login.register, name='register'),
 
 ]
--- LMSapp/views.py
+++ LMSapp/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def BASE(request):
-    return render(request,'base.html')
+    return render(request, 'base.html')
+
+
+def home(request):
+    return render(request, 'Main/home.html')
+
+
+def singlecourse(request):
+    return render(request, 'Main/singlecourse.html')
+
+
+def contactus(request):
+    return render(request, 'Main/contactus.html')
+
+
+def aboutus(request):
+    return render(request, 'Main/aboutus.html')
+
+
