a882618c2f76e8c81a325ae41f427e2e156ac310
SinghParmjot <42267558+parmjotsingh@users.noreply.github.com>
2023-07-18 21:35:36 -0400

    additional functionality realted to login, forget password, home, courses and admin

--- LMSapp/urls.py
+++ LMSapp/urls.py
@@ -1,14 +1,19 @@
 from . import views, user_login
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('base', views.BASE, name='BASE'),
-    path('', views.home, name='home'),
-    path('singlecourse', views.singlecourse, name='singlecourse'),
-    path('contactus', views.contactus, name='contactus'),
-    path('aboutus', views.aboutus, name='aboutus'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register', user_login.register, name='register'),
-
-]
+                  path('base', views.BASE, name='BASE'),
+                  path('', views.home, name='home'),
+                  path('singlecourse', views.singlecourse, name='singlecourse'),
+                  path('contactus', views.contactus, name='contactus'),
+                  path('aboutus', views.aboutus, name='aboutus'),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('accounts/register', user_login.REGISTER, name='register'),
+                  path('accounts/doLogin', user_login.DO_LOGIN, name='doLogin'),
+                  path('accounts/profile', user_login.PROFILE, name='profile'),
+                  path('accounts/profile_update', user_login.Profile_Update, name='profileUpdate'),
+                  path('admin/', admin.site.urls)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
