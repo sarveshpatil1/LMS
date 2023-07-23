d0287c8cd47e08b7f0d0f8ca1d05a924753bc825
sarveshpatil1 <sarveshpatil0000@gmail.com>
2023-07-23 01:16:16 -0400

    added data and cookies

--- LMSapp/views.py
+++ LMSapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import *
 from django.template.loader import render_to_string
-from django.http import JsonResponse
+from django.http import JsonResponse, response, HttpResponse, HttpResponseRedirect
 from django.db.models import Sum
 from django.contrib import messages
 from .forms import Course_Detail_Form
@@ -15,7 +17,9 @@ def home(request):
     categories = Categories.objects.all().order_by('id')[0:5]
     course = Course.objects.filter(status='PUBLISH').order_by('-id')
     context = {"category": categories, "course": course}
-    return render(request, 'Main/home.html', context)
+    response = render(request, 'Main/home.html', context)
+    response.delete_cookie('user_login_enrollment_msg')
+    return response
 
 
 def singlecourse(request):
@@ -98,11 +102,11 @@ def coursedetails(request, slug):
     course_id = Course.objects.get(slug=slug)
     membership = request.user.groups.filter(name="gold members").exists()
     print(membership)
+    user_login_enrollment_msg = request.COOKIES.get('user_login_enrollment_msg')
     try:
         check_enroll = UserCourse.objects.get(user=request.user, course=course_id)
     except Exception as e:
         check_enroll = None
-
 
     course = Course.objects.filter(slug=slug)
     if course.exists():
@@ -115,7 +119,8 @@ def coursedetails(request, slug):
         'category': category,
         'time_duration': time_duration,
         'enrol_status': check_enroll,
-        'gold_member':membership
+        'gold_member': membership,
+        'user_login_enrollment_msg': user_login_enrollment_msg
     }
     return render(request, 'course/course_details.html', context)
 
@@ -130,27 +135,37 @@ def pagenotfound(request):
 
 
 def checkout(request, slug):
-    course = Course.objects.get(slug=slug)
-    context = {'course': course}
+    if request.user.is_authenticated:
+        membership = request.user.groups.filter(name="gold members").exists()
+        course = Course.objects.get(slug=slug)
+        context = {'course': course, 'gold_member': membership}
 
-    if course.price == 0:
-        course = UserCourse(
-            user=request.user,
-            course=course,
-        )
-        course.save()
-        messages.success(request, 'Success you have enrolled in the course')
-        return redirect('mycourse')
-    elif request.method == "POST":
-        name = request.POST.get('first_name')
-        course = UserCourse(
-            user=request.user,
-            course=course,
-        )
-        course.save()
-        messages.success(request, 'payment successful ' + str(name) + ' you have enrolled in the course ' + str(slug))
-        return redirect('mycourse')
-    return render(request, 'checkout/checkout.html', context)
+        if course.price == 0:
+            course = UserCourse(
+                user=request.user,
+                course=course,
+            )
+            course.save()
+            messages.success(request, 'Success you have enrolled in the course')
+            return redirect('mycourse')
+        elif request.method == "POST":
+            name = request.POST.get('first_name')
+            course = UserCourse(
+                user=request.user,
+                course=course,
+            )
+            course.save()
+            messages.success(request,
+                             'payment successful ' + str(name) + ' you have enrolled in the course ' + str(slug))
+            return redirect('mycourse')
+        return render(request, 'checkout/checkout.html', context)
+    else:
+        response = redirect('coursedetails', slug)
+        msg = "You need to login into your account before enrolling"
+        response.set_cookie('user_login_enrollment_msg', msg, max_age=15)
+        return response
+
+    return redirect('coursedetails', slug)
 
 
 def mycourse(request):
@@ -202,5 +217,4 @@ def register_course_details(request):
 
 
 def auther_course_details(request):
-
     return render(request, "course/author_course_detail.html")
