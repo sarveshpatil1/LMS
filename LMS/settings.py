ed42f73deff7f30d5d55e6f8ccd6b1782e9140a0
sarvesh patil <50295990+sarveshpatil1@users.noreply.github.com>
2023-07-24 02:20:13 -0400

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -138,5 +138,5 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sampeterson099@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgvemdeznmxbbzxl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
