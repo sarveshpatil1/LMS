dfaee08d0509b29945468f4d876e7736aa6078bb
sarveshpatil1 <sarveshpatil0000@gmail.com>
2023-07-23 20:35:26 -0400

    final changes

--- LMS/settings.py
+++ LMS/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,12 +130,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.freesmtpservers.com'  # 'smtp.gmail.com'
-EMAIL_PORT = 25  # 587
-EMAIL_HOST_USER = 'enter email'
-# EMAIL_HOST_PASSWORD = 'pass'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sampeterson099@gmail.com'
+EMAIL_HOST_PASSWORD = 'pgvemdeznmxbbzxl'
